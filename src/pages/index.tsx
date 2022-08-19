a6c8aafc673ac78ffd427f6b5940f033bcd391d0
Raquel <raquelfernandassilva@hotmail.com>
2022-08-19 16:25:52 -0300

    feat: page post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,65 @@
-import { format } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import { getPrismicClient } from '../services/prismic';
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
-import Post from './post/[slug]';
+import { format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import Link from 'next/link'
+import { getPrismicClient } from '../services/prismic'
+import styles from './home.module.scss'
+import Post from './post/[slug]'
 
 interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
+    uid?: string
+    first_publication_date: string | null
+    data: {
+        title: string
+        subtitle: string
+        author: string
+    }
 }
 
 interface PostPagination {
-  next_page: string;
-  results: Post[];
+    next_page: string
+    results: Post[]
 }
 
 interface HomeProps {
-  postsPagination: PostPagination;
+    postsPagination: PostPagination
 }
 
 export default function Home({ posts }) {
-  const formatDate = date => {
-    return format(new Date(date), 'dd MMM yyyy', { locale: ptBR });
-  };
+    const formatDate = (date) => {
+        return format(new Date(date), 'dd MMM yyyy', { locale: ptBR })
+    }
 
-  console.log(posts);
-  return (
-    <main className={styles.allPage}>
-      {posts?.map(({ data, last_publication_date }) => (
-        <Link href={`'/' ${data}`}>
-          <div className={styles.cardPost}>
-            <strong className={styles.title}>{data.title}</strong>
-            <p>{data.subtitle}</p>
-            <span>
-              <time>
-                <img src="/calendar.svg" alt="calendario" />
-                {formatDate(last_publication_date)}
-              </time>
-              <img src="/user.svg" alt="usuario" />
-              <p>{data.author}</p>
-            </span>
-          </div>
-        </Link>
-      ))}
-      <button className={styles.buttonLoadMore}>Carregar mais posts</button>
-    </main>
-  );
+    return (
+        <main className={styles.allPage}>
+            {posts?.map(({ data, last_publication_date, uid }) => (
+                <Link href={`/post/${uid}`}>
+                    <div className={styles.cardPost}>
+                        <strong className={styles.title}>{data.title}</strong>
+                        <p>{data.subtitle}</p>
+                        <span>
+                            <time>
+                                <img src="/calendar.svg" alt="calendario" />
+                                {formatDate(last_publication_date)}
+                            </time>
+                            <img src="/user.svg" alt="usuario" />
+                            <p>{data.author}</p>
+                        </span>
+                    </div>
+                </Link>
+            ))}
+            <button className={styles.buttonLoadMore}>
+                Carregar mais posts
+            </button>
+        </main>
+    )
 }
 
 export const getStaticProps = async () => {
-  const prismic = getPrismicClient();
-  const posts = await prismic.getByType('posts');
+    const prismic = getPrismicClient()
+    const posts = await prismic.getByType('posts')
 
-  return {
-    props: { posts: posts.results },
-  };
-};
+    return {
+        props: { posts: posts.results },
+    }
+}
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,53 +1,104 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
-
-import { getPrismicClient } from '../../services/prismic';
-
-import commonStyles from '../../styles/common.module.scss';
-import styles from './post.module.scss';
-
+import { getPrismicClient } from '../../services/prismic'
+import style from '../../pages/post/post.module.scss'
+import { RichText } from 'prismic-dom'
+import { PrismicRichText } from '@prismicio/react'
+import Image from 'next/image'
 interface Post {
-  first_publication_date: string | null;
-  data: {
-    title: string;
+    first_publication_date: string | null
+    title: string
     banner: {
-      url: string;
-    };
-    author: string;
+        url: string
+    }
+    author: string
     content: {
-      heading: string;
-      body: {
-        text: string;
-      }[];
-    }[];
-  };
+        heading: string
+        body: {
+            text: string
+        }[]
+    }[]
 }
 
 interface PostProps {
-  post: Post;
+    post: Post
 }
 
-export default function Post() {
-  return (
-    <>
-      <h1>teest</h1>
-    </>
-  );
+export default function Post({ post }: PostProps) {
+    return (
+        <>
+            <div className={style.container}>
+                <img
+                    src={post.banner}
+                    alt="img"
+                    layout="fill"
+                    className={style.img}
+                />
+                <div className={style.teste}>
+                    <div>
+                        <h1 className={style.title}>{post.title}</h1>
+                    </div>
+                    <div className={style.inf}>
+                        <div className={style.date}>
+                            <div>
+                                <img
+                                    src="/calendar.png"
+                                    width="20px"
+                                    height="20px"
+                                />
+                                {post.first_publication_date}
+                            </div>
+                        </div>
+                        <div>
+                            <img src="/user.png" alt="" />
+                            {post.author}
+                        </div>
+                    </div>
+                    <div className={style.text}>
+                        <h2 className={style.text}>{post.subtitle}</h2>
+                        {post.content.map((content) => (
+                            <>
+                                <h5>{content.heading}</h5>
+                                <h5>
+                                    <PrismicRichText field={content.body} />
+                                </h5>
+                            </>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export const getStaticPaths = () => {
-  // const prismic = getPrismicClient({});
-  // const posts = await prismic.getByType('posts');
-  // return {
-  //   props: { posts },
-  // };
-};
+    return {
+        paths: [],
+        fallback: 'blocking',
+    }
+}
 
-export const getStaticProps = async () => {
-  const prismic = getPrismicClient({});
-  const posts = await prismic.getByType('posts');
-  if (process.env.NODE_ENV === 'production') {
-    return { notFound: true };
-  } else {
-    return { props: { posts } };
-  }
-};
+export const getStaticProps = async ({ req, params }) => {
+    const { slug } = params
+    const prismic = getPrismicClient(req)
+    const response = await prismic.getByUID('posts', String(slug), {})
+    console.log(response)
+    const post = {
+        slug,
+        title: response.data.title,
+        subtitle: response.data.subtitle,
+        content: response.data.content,
+        banner: response.data.banner.url,
+        author: response.data.author,
+        first_publication_date: new Date(
+            response.last_publication_date
+        ).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+        }),
+    }
+    return {
+        props: {
+            post,
+        },
+    }
+}
